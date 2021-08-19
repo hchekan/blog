188d88e340951f270c3a56bf7d11df158336c173
Halina Chekan <HChekan@gomel.iba.by>
2021-08-19 17:13:00 +0300

    fix scroll

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //burger-menu
 const sticks = document.querySelector('#contain');
+const body = document.querySelector('#body');
 sticks.addEventListener('click', function (e) {
   e.preventDefault();
   const modal = document.querySelector('.burger__nav');
@@ -8,12 +9,14 @@ sticks.addEventListener('click', function (e) {
   const nav_items = document.querySelector('#nav');
   nav_items.classList.toggle('open');
   sticks.classList.toggle('change');
+  body.classList.toggle('overflow');
 
   window.onclick = function(event) {
     if (event.target === modal) {
       modal.classList.toggle('block');
       nav_items.classList.toggle('open');
       sticks.classList.toggle('change');
+      body.classList.toggle('overflow');
     }
   };
 });
